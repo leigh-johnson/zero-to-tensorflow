07b8db23d0846fbd50d88361ef71860ace25a4e3
Leigh Johnson <hi@leighjohnson.me>
2019-05-28 11:51:08 -0700

    fix deps

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['gym[atari]', 'tensorflow==2.0.0-alpha0']
+requirements = ['gym[atari]', 'tensorflow==2.0.0-alpha0', 'numpy']
 
 setup_requirements = ['pytest-runner', ]
 
