14bce4accf2ed5d0eed56544669380fb098e7659
Leigh Johnson <hi@leighjohnson.me>
2019-06-02 12:46:22 -0700

    adjust final layer loss function to use processed_rewards * cross_entropy values, without a movement penalty rollout reward sum is now a running total of std(rewards), instead of 90% running total of raw unprocessed reward sum

--- rl-skiing/trainer/task.py
+++ rl-skiing/trainer/task.py
@@ -78,100 +78,100 @@ def main(args):
     args_dict = vars(args)
     logging.info('args: {}'.format(args_dict))
 
-    with tf.device('/device:GPU:0'):
-        with tf.Graph().as_default() as g:
-            # rollout subgraph
-            with tf.name_scope('rollout'):
-                observations = tf.placeholder(
-                    shape=(None, OBSERVATION_DIM), dtype=tf.float32)
+    with tf.Graph().as_default() as g:
+        # rollout subgraph
+        with tf.name_scope('rollout'):
+            observations = tf.placeholder(
+                shape=(None, OBSERVATION_DIM), dtype=tf.float32)
 
-                logits = build_graph(observations)
+            logits = build_graph(observations)
 
-                logits_for_sampling = tf.reshape(
-                    logits, shape=(1, len(ACTIONS)))
+            logits_for_sampling = tf.reshape(
+                logits, shape=(1, len(ACTIONS)))
 
-                # Sample the action to be played during rollout.
-                sample_action = tf.squeeze(tf.multinomial(
-                    logits=logits_for_sampling, num_samples=1), )
+            # Sample the action to be played during rollout.
+            sample_action = tf.squeeze(tf.multinomial(
+                logits=logits_for_sampling, num_samples=1), )
+
+        # dataset subgraph for experience replay
+        with tf.name_scope('dataset'):
+            # the dataset reads from MEMORY
+            ds = tf.data.Dataset.from_generator(
+                gen, output_types=(tf.float32, tf.int32, tf.float32))
+            ds = ds.shuffle(MAX_MEMORY_LEN).repeat().batch(args.batch_size)
+            iterator = ds.make_one_shot_iterator()
+
+        # training subgraph
+        with tf.name_scope('train'):
+            # the train_op includes getting a batch of data from the dataset, so we do not need to use a feed_dict when running the train_op.
+            next_batch = iterator.get_next()
+            train_observations, labels, processed_rewards = next_batch
+
+            # This reuses the same weights in the rollout phase.
+            train_observations.set_shape(
+                (args.batch_size, OBSERVATION_DIM))
+            train_logits = build_graph(train_observations)
+
+            cross_entropies = tf.nn.sparse_softmax_cross_entropy_with_logits(
+                logits=train_logits,
+                labels=labels
+            )
 
             optimizer = tf.train.RMSPropOptimizer(
                 learning_rate=args.learning_rate,
                 decay=args.rmsprop_decay
             )
 
-            # dataset subgraph for experience replay
-            with tf.name_scope('dataset'):
-                # the dataset reads from MEMORY
-                ds = tf.data.Dataset.from_generator(
-                    gen, output_types=(tf.float32, tf.int32, tf.float32))
-                ds = ds.shuffle(MAX_MEMORY_LEN).repeat().batch(args.batch_size)
-                iterator = ds.make_one_shot_iterator()
+            # Extra loss when the paddle is moved, to encourage more natural moves.
+            # probs = tf.nn.softmax(logits=train_logits)
+            # move_cost = args.move_penalty * \
+            #     tf.reduce_sum(probs * [a['penalty']
+            #                            for a in ACTIONS.values()], axis=1)
 
-            # training subgraph
-            with tf.name_scope('train'):
-                # the train_op includes getting a batch of data from the dataset, so we do not need to use a feed_dict when running the train_op.
-                next_batch = iterator.get_next()
-                train_observations, labels, processed_rewards = next_batch
+            #logging.info(f'move_cost {move_cost}')
+            loss = tf.reduce_sum(processed_rewards * cross_entropies)
 
-                # This reuses the same weights in the rollout phase.
-                train_observations.set_shape(
-                    (args.batch_size, OBSERVATION_DIM))
-                train_logits = build_graph(train_observations)
+            global_step = tf.train.get_or_create_global_step()
 
-                cross_entropies = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                    logits=train_logits,
-                    labels=labels
-                )
+            train_op = optimizer.minimize(loss, global_step=global_step)
 
-                # Extra loss when the paddle is moved, to encourage more natural moves.
-                probs = tf.nn.softmax(logits=train_logits)
-                move_cost = args.move_penalty * \
-                    tf.reduce_sum(probs * [a['penalty']
-                                           for a in ACTIONS.values()], axis=1)
-                loss = tf.reduce_sum(processed_rewards *
-                                     cross_entropies + move_cost)
+        init = tf.global_variables_initializer()
+        saver = tf.train.Saver(max_to_keep=args.max_to_keep)
 
-                global_step = tf.train.get_or_create_global_step()
+        with tf.name_scope('summaries'):
+            rollout_reward = tf.placeholder(
+                shape=(),
+                dtype=tf.float32
+            )
 
-                train_op = optimizer.minimize(loss, global_step=global_step)
+            # the weights to the hidden layer can be visualized
+            hidden_weights = tf.trainable_variables()[0]
+            for h in range(args.hidden_dim):
+                slice_ = tf.slice(hidden_weights, [0, h], [-1, 1])
+                image = tf.reshape(
+                    slice_, [1, OBSERVATION_DIM_Y, OBSERVATION_DIM_X, 1])
+                tf.summary.image('hidden_{:04d}'.format(h), image)
 
-            init = tf.global_variables_initializer()
-            saver = tf.train.Saver(max_to_keep=args.max_to_keep)
+            for var in tf.trainable_variables():
+                tf.summary.histogram(var.op.name, var)
+                tf.summary.scalar('{}_max'.format(
+                    var.op.name), tf.reduce_max(var))
+                tf.summary.scalar('{}_min'.format(
+                    var.op.name), tf.reduce_min(var))
 
-            with tf.name_scope('summaries'):
-                rollout_reward = tf.placeholder(
-                    shape=(),
-                    dtype=tf.float32
-                )
+            tf.summary.scalar('rollout_reward', rollout_reward)
+            tf.summary.scalar('loss', loss)
 
-                # the weights to the hidden layer can be visualized
-                hidden_weights = tf.trainable_variables()[0]
-                for h in range(args.hidden_dim):
-                    slice_ = tf.slice(hidden_weights, [0, h], [-1, 1])
-                    image = tf.reshape(
-                        slice_, [1, OBSERVATION_DIM_Y, OBSERVATION_DIM_X, 1])
-                    tf.summary.image('hidden_{:04d}'.format(h), image)
+            merged = tf.summary.merge_all()
 
-                for var in tf.trainable_variables():
-                    tf.summary.histogram(var.op.name, var)
-                    tf.summary.scalar('{}_max'.format(
-                        var.op.name), tf.reduce_max(var))
-                    tf.summary.scalar('{}_min'.format(
-                        var.op.name), tf.reduce_min(var))
+        logging.info('Number of trainable variables: {}'.format(
+            len(tf.trainable_variables())))
 
-                tf.summary.scalar('rollout_reward', rollout_reward)
-                tf.summary.scalar('loss', loss)
-
-                merged = tf.summary.merge_all()
-
-            logging.info('Number of trainable variables: {}'.format(
-                len(tf.trainable_variables())))
-
-            inner_env = gym.make('Skiing-v0')
-            # tf.agents helper to more easily track consecutive pairs of frames
-            env = FrameHistory(inner_env, past_indices=[0, 1], flatten=False)
-            # tf.agents helper to automatically reset the environment
-            env = AutoReset(env)
+        inner_env = gym.make('Skiing-v0')
+        # tf.agents helper to more easily track consecutive pairs of frames
+        env = FrameHistory(inner_env, past_indices=[0, 1], flatten=False)
+        # tf.agents helper to automatically reset the environment
+        env = AutoReset(env)
 
     with tf.Session(graph=g, config=tf.ConfigProto(log_device_placement=True, allow_soft_placement=True)) as sess:
         if args.restore:
@@ -203,8 +203,7 @@ def main(args):
                                    observations: [_observation]})
 
                 # _label = ACTIONS[_action]
-
-                _pair_state, _reward, _done, _ = env.step(_action)
+                _pair_state, _reward, _done, _lives = env.step(_action)
 
                 if args.render:
                     env.render()
@@ -213,9 +212,9 @@ def main(args):
                 episode_memory.append((_observation, _action, _reward))
 
                 # Get processed frame delta for the next step
-                pair_state = _pair_state
+                # pair_state = _pair_state
 
-                current_state, previous_state = pair_state
+                current_state, previous_state = _pair_state
                 current_x = preprocess_frame(current_state)
                 previous_x = preprocess_frame(previous_state)
 
@@ -223,7 +222,6 @@ def main(args):
 
                 if _done:
                     obs, lbl, rwd = zip(*episode_memory)
-
                     # processed rewards
                     prwd = discount_rewards(
                         rwd, decay_factor=args.reward_decay)
@@ -235,7 +233,7 @@ def main(args):
 
                     # calculate the running rollout reward
 
-                    _rollout_reward = 0.9 * _rollout_reward + 0.1 * sum(rwd)
+                    _rollout_reward = _rollout_reward + sum(prwd)
                     episode_memory = []
 
                     # if args.render:
@@ -343,8 +341,8 @@ def parse_args():
     parser.add_argument(
         '--move-penalty',
         type=float,
-        default=0.99,
-        help='additional penalty (loss function multipler) applied when actor is moved, which discourages super-human bursts of movement'
+        default=0.1,
+        help='additional penalty, which discourages super-human bursts of movement'
     )
     parser.add_argument(
         '--hidden-dim',
